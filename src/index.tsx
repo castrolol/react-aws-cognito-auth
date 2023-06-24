a5f818ac436f29b641e7ce57a5b4f332304c7d5f
castrolol <lu4n.k4str0@gmail.com>
2023-06-24 18:03:27 -0400

    Adding more fns

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
-export { ReactCognitoAuthConfig, login, logout } from './react-cognito-auth';
+export * from './react-cognito-auth';
 export type { User } from './react-cognito-auth-atom';
 export { ReactCognitoAuthProvider, useAuth } from './react-cognito-auth-provider';
